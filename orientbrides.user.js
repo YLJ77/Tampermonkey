e7f8fe20fe978da5fc513d6b29f4f60c8139c220
YLJ77 <564296557@qq.com>
2017-05-16 18:10:11 +0800

    添加并发收集ID和发信功能

--- orientbrides.user.js
+++ orientbrides.user.js
@@ -13,8 +13,10 @@ function CollectId() {
   this.renderBase();
   
   if(localStorage.awaitPendingTimeout === 'true') {
+    let refreshTimes = +localStorage.refreshTimes, selector;
+    localStorage.refreshTimes = ++refreshTimes;
     localStorage.awaitPendingTimeout = 'false';
-    let selector = (localStorage.theTypeOfTaskBeingPerformed === 'collectId') ? 'collectId' : 'sendEmail';
+    selector = localStorage.theTypeOfTaskBeingPerformed;
     selector = `#${selector}`;
     this.findEle(selector).then($ele=>{
       $ele.get(0).click();
@@ -23,10 +25,8 @@ function CollectId() {
 }
 
 CollectId.prototype = {
-  emailMaxId: 11400000,
   ladyId: 1187357,
   requestTimeSpan: 100,
-  notifyTimeSpan: 2000,
   collectMaxId: 33914386,
   curId: 11111111,
   pendingLimit: 30,
@@ -55,16 +55,18 @@ CollectId.prototype = {
     });
   },
   renderBase() {
-    let $bar = $("<div><button class='btn btn-default' type='button' id='collectId'>开始收集id</button><button class='btn btn-default' type='button' id='sendEmail'>开始发信</button></div>")
+    let $bar = $(`<div><button class='btn btn-default' type='button' id='collectId'>开始收集id</button><br>
+                  <button class='btn btn-default' type='button' id='sendEmail'>开始发信</button><br>
+                  <button class='btn btn-default' type='button' id='collectIdSendEmail'>收集ID且发信</button></div>`)
     .css({
       position: 'fixed',
-      top: '70px',
+      top: '100px',
       left: '20px',
       'z-index': 999
     })
     .find('button')
     .css({
-      width: '70px',
+      width: '150px',
       height: '30px'
     })
     .end()
@@ -73,17 +75,36 @@ CollectId.prototype = {
     .end()
     .find('#sendEmail')
     .on('click',this.controllEmail.bind(this))
+    .end()
+    .find('#collectIdSendEmail')
+    .on('click',this.cCollectSend.bind(this))
     .end();
 
     $('body').prepend($bar);
   },
+  cCollectSend(e) {
+    let $ele = $(e.target);
+    let idArray = localStorage.idArray;
+    window.idArray = JSON.parse(idArray);
+    $('#collectId').css('display', 'none');
+    $('#sendEmail').css('display', 'none');
+    if ($ele.text() === '收集ID且发信') {
+      localStorage.theTypeOfTaskBeingPerformed = 'collectIdSendEmail';
+      $ele.text('暂停收集ID且发信');
+      this.mutilThreadCollect(true);
+    } else if ($ele.text() === '暂停收集ID且发信') {
+      $ele.text('收集ID且发信');
+      clearInterval(this.clock);
+    }
+  },
   controlCollect(e) {
     let $ele = $(e.target);
     $('#sendEmail').css('display', 'none');
+    $('#collectIdSendEmail').css('display', 'none');
     if ($ele.text() === '开始收集id') {
       localStorage.theTypeOfTaskBeingPerformed = 'collectId';
       $ele.text('暂停收集id');
-      this.mutilThread(this.doCollect, 'collectId');
+      this.mutilThreadCollect();
     } else if ($ele.text() === '暂停收集id') {
       $ele.text('开始收集id');
       clearInterval(this.clock);
@@ -94,10 +115,11 @@ CollectId.prototype = {
     let idArray = localStorage.idArray;
     window.idArray = JSON.parse(idArray);
     $('#collectId').css('display', 'none');
+    $('#collectIdSendEmail').css('display', 'none');
     if ($ele.text() === '开始发信') {
       localStorage.theTypeOfTaskBeingPerformed = 'sendEmail';
       $ele.text('暂停发信');
-      this.mutilThread(this.doEmail, 'sendEmail');
+      this.mutilThreadEmail();
     } else if ($ele.text() === '暂停发信') {
       $ele.text('开始发信');
       clearInterval(this.clock);
@@ -129,6 +151,7 @@ CollectId.prototype = {
     localStorage.pendingAmount = 0;
     localStorage.getNamePendingAmount = 0;
     localStorage.getNameXhrFailTimes = 0;
+    localStorage.refreshTimes = 0;
     localStorage.holdingPendingTimes = this.awaitPendingTimeoutLimit();
     localStorage.getNameHoldingPendingTimes = this.awaitPendingTimeoutLimit();
     if(localStorage.idArray === undefined) {
@@ -147,87 +170,111 @@ CollectId.prototype = {
     return new Promise((resolve, reject)=>{
       let url = this.getUrl(id);
       $.ajax({url:url,type:'GET'}).done(((data,statusText)=>{
+        if (typeof data !== 'string') reject('fail');
         let startIndex = data.indexOf(this.nameFlag);
         startIndex += this.nameFlag.length;
         let endIndex = data.indexOf('</td>', startIndex);
         let name = data.substring(startIndex, endIndex).replace(/\s*<td>/, '').split(' ')[0];
-        resolve(name);
+        if (name !== '') {
+          resolve(name);
+        } else {
+          reject('fail');
+        }
+        
       })).fail((xhr)=>{
         this.handleFail(xhr, 'getNameXhrFailTimes', this.pendingLimit, id, this.refresh);
         reject('fail');
       }).always(()=>{
         let getNamePendingAmount = +localStorage.getNamePendingAmount;
-        localStorage.getNamePendingAmount = --getNamePendingAmount;
+        if(getNamePendingAmount > 0) localStorage.getNamePendingAmount = --getNamePendingAmount;
+        this.resetHoldingPendingTimes('getNameHoldingPendingTimes');
       });
     });
   },
   handleReachPendingLimit(field) {
-    let pendingTimes = +localStorage[field];
-    localStorage[field] = --pendingTimes;
-    if (pendingTimes === 0) this.refresh();
+    let pendingAmount = +localStorage[field];
+    localStorage[field] = --pendingAmount;
+    if (pendingAmount === 0) this.refresh();
   },
-  mutilThread(fn, type) {
+  resetHoldingPendingTimes(field) {
+    localStorage[field] = this.awaitPendingTimeoutLimit();
+  },
+  mutilThreadEmail() {
+    let timeSpan = this.requestTimeSpan;
     this.clock = setInterval(()=>{
       let pendingAmount = +localStorage.pendingAmount;
-      let holdingPendingTimes, maxId, curId, selector, btnText, completeText, nexId, letter, letterCurIndex, letterArr, getNamePendingAmount, emailCurIdIndex;
-      if (type === 'sendEmail') {
-        curId = this.getCurId('sendEmail');
-        selector = '#sendEmail';
-        btnText = '开始发信';
-        completeText = '邮件已发完';
-      } else if (type === 'collectId') {
-        curId = this.getCurId('collectId');
-        maxId = this.collectMaxId;
-        selector = '#collectId';
-        btnText = '开始收集id';
-        completeText = '收集ID完成';
-      }
-      
+      let curId, selector, btnText, completeText, letter, letterCurIndex, letterArr, getNamePendingAmount, emailCurIdIndex;
+      curId = this.getCurId('sendEmail');
+      selector = '#sendEmail';
+      btnText = '开始发信';
+      completeText = '邮件已发完毕';
+      emailCurIdIndex = +localStorage.emailCurIdIndex;
       if (pendingAmount <= this.pendingLimit) {
+        if (emailCurIdIndex > window.idArray.length) {
+          clearInterval(this.clock);
+          alert(completeText);
+          $(selector).text(btnText);
+          return;
+        }
         localStorage.pendingAmount = ++pendingAmount;
-      } else {
-        this.handleReachPendingLimit('holdingPendingTimes');
-      }
-      
-      console.log(`已开始 ${curId}`);
-      if (type === 'sendEmail') {
+        emailCurIdIndex = +localStorage.emailCurIdIndex;
+        localStorage.emailCurIdIndex = ++emailCurIdIndex;
+        
+        console.log(`已开始 ${curId}`);
         getNamePendingAmount = +localStorage.getNamePendingAmount;
         letterCurIndex = +localStorage.letterCurIndex;
         letterArr = JSON.parse(localStorage.letterArr);
         letter = letterArr[letterCurIndex];
         if (getNamePendingAmount <= this.pendingLimit) {
           localStorage.getNamePendingAmount = ++getNamePendingAmount;
+          this.getManName(curId).then(manName=>{
+            if (typeof manName !== 'string' || manName === 'fail') return;
+            letter = this.generateLetter(letter, manName, this.ladyName);
+            this.doEmail(curId, letter);
+          });
         } else {
+          console.log('getNamePending');
           this.handleReachPendingLimit('getNameHoldingPendingTimes');
         }
-        this.getManName(curId).then(manName=>{
-          if (typeof manName !== 'string' || manName === 'fail') return;
-          letter = this.generateLetter(letter, manName, this.ladyName);
-          fn.bind(this, curId, letter)();
-        });
-        emailCurIdIndex = +localStorage.emailCurIdIndex;
-        localStorage.emailCurIdIndex = ++emailCurIdIndex;
-        if (emailCurIdIndex > window.idArray.length) {
-          alert('邮件已发完毕');
-          return;
-        }
         
-      } else if (type === 'collectId') {
-        if (curId > maxId) {
-          clearInterval(this.clock);
-          $(selector).text(btnText);
-          alert(completeText);
-          return;
-        }
-        fn.bind(this, curId)();
+      } else {
+        console.log('sendingEmailPending');
+        this.handleReachPendingLimit('holdingPendingTimes');
+      }
+    }, timeSpan);
+  },
+  mutilThreadCollect(sendEmail) {
+    let timeSpan = this.requestTimeSpan;
+    this.clock = setInterval(()=>{
+      let pendingAmount = +localStorage.pendingAmount;
+      let maxId, curId, selector, btnText, completeText, nexId;
+      curId = this.getCurId('collectId');
+      maxId = this.collectMaxId;
+      selector = '#collectId';
+      btnText = '开始收集id';
+      completeText = '收集ID完成';
+      
+      if (pendingAmount <= this.pendingLimit) {
+        localStorage.pendingAmount = ++pendingAmount;
         nexId = this.increaseId(curId);
         this.saveCurId(nexId);
+        console.log(`已开始 ${curId}`);
+        if (curId > maxId) {
+            clearInterval(this.clock);
+            $(selector).text(btnText);
+            alert(completeText);
+            return;
+          }
+        this.doCollect(curId, sendEmail);
+        
+      } else {
+        console.log('collectIdPending');
+        this.handleReachPendingLimit('holdingPendingTimes');
       }
       
-      
-    }, this.requestTimeSpan);
+    }, timeSpan);
   },
-  doEmail(id,letter) {
+  doEmail(id,letter, sendEmail) {
     let url = this.getUrl(id);
     let ladyId = this.ladyId;
     $.ajax({
@@ -246,36 +293,54 @@ CollectId.prototype = {
       ctl00$ctl00$ctl00$ContentPlaceHolder1$nestedContentPlaceHolder$ContentIndex$cntrlViewLadyCorrespondence$btnLetterReplySend:'Send'
       }
     }).done((data,statusText)=>{
+      localStorage.xhrFailTimes = 0;
       console.info(`发送信件(${id}): ${statusText}`);
+      if (sendEmail === true) this.msg('Collect Id && Send Email', `已存储且已发送 ${id}`);
     }).fail(xhr=>{
       console.info(`发送信件: ${xhr.statusText}`);
       this.handleFail(xhr, 'xhrFailTimes', this.pendingLimit, id, this.refresh);
     }).always(()=>{
       let pendingAmount = +localStorage.pendingAmount;
-      localStorage.pendingAmount = --pendingAmount;
+      if(pendingAmount > 0) localStorage.pendingAmount = --pendingAmount;
+      this.resetHoldingPendingTimes('holdingPendingTimes');
     });
   },
-  doCollect(id) {
+  doCollect(id, sendEmail) {
     let url = this.getUrl(id);
     $.ajax({url:url,type:'GET'}).done(((data,statusText)=>{
       console.log(`${id}: ${statusText}`);
-      let idArray,curId,nextId,idJSON,cantWriteIndex,canWriteIndex,noInfoIndex;
+      let idArray,nextId,idJSON,cantWriteIndex,canWriteIndex,noInfoIndex, startIndex, endIndex, manName, letterCurIndex, letterArr, letter, pendingAmount, emailCurIdIndex;
       let cantWriteFlag = this.cantWriteFlag;
       localStorage.xhrFailTimes = 0;
-      localStorage.holdingPendingTimes = this.awaitPendingTimeoutLimit();
       if (typeof data === 'string') {
         cantWriteIndex = data.indexOf(cantWriteFlag);
         canWriteIndex = data.indexOf(this.canWriteFlag);
         noInfoIndex = data.indexOf(this.noInfoFlag);
         console.info(`cantWriteFlag: ${cantWriteIndex}`);
         if(cantWriteIndex === -1 && canWriteIndex !== -1 && noInfoIndex === -1) {
-        idArray = JSON.parse(localStorage.idArray);
-        idArray.push(id);
-        localStorage.idArrayLength = idArray.length;
-        idJSON = JSON.stringify(idArray);
-        this.saveIdArray(idJSON);
-        this.msg('Collect Id', `已存储 ${id}`);
-        console.warn(`已存储 ${id}`);
+          idArray = JSON.parse(localStorage.idArray);
+          idArray.push(id);
+          localStorage.idArrayLength = idArray.length;
+          idJSON = JSON.stringify(idArray);
+          this.saveIdArray(idJSON);
+          if (sendEmail === undefined) this.msg('Collect Id', `已存储 ${id}`);
+          if (sendEmail === true) {  //是否需要发信
+            emailCurIdIndex = +localStorage.emailCurIdIndex;
+            localStorage.emailCurIdIndex = ++emailCurIdIndex;
+            startIndex = data.indexOf(this.nameFlag);
+            startIndex += this.nameFlag.length;
+            endIndex = data.indexOf('</td>', startIndex);
+            manName = data.substring(startIndex, endIndex).replace(/\s*<td>/, '').split(' ')[0];
+            if (manName !== '') {
+              pendingAmount = +localStorage.pendingAmount;
+              localStorage.pendingAmount = ++pendingAmount;
+              letterCurIndex = +localStorage.letterCurIndex;
+              letterArr = JSON.parse(localStorage.letterArr);
+              letter = letterArr[letterCurIndex];
+              letter = this.generateLetter(letter, manName, this.ladyName);
+              this.doEmail(id, letter, true);
+            }
+          }
         } else {
           console.warn(`已跳过 ${id}`);
         }
@@ -286,7 +351,8 @@ CollectId.prototype = {
       this.handleFail(xhr, 'xhrFailTimes', this.pendingLimit, id, this.refresh);
     }).always(()=>{
       let pendingAmount = +localStorage.pendingAmount;
-      localStorage.pendingAmount = --pendingAmount;
+      if(pendingAmount > 0) localStorage.pendingAmount = --pendingAmount;
+      this.resetHoldingPendingTimes('holdingPendingTimes');
     });
   },
   handleFail(xhr, field, limit, id, fn) {
@@ -298,9 +364,6 @@ CollectId.prototype = {
   },
   refresh() {
     clearInterval(this.clock);
-    let refreshTimes = localStorage.refreshTimes === undefined ? 0 : +localStorage.refreshTimes;
-    refreshTimes += 1;
-    localStorage.refreshTimes = refreshTimes;
     localStorage.awaitPendingTimeout = 'true';
     window.location.reload(true);
   },
