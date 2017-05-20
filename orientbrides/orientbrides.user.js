8150399a3feaa6b91157e44cbabdad93e7bba78d
YLJ77 <564296557@qq.com>
2017-05-20 23:26:25 +0800

    兼容手机设置

--- orientbrides/orientbrides.user.js
+++ orientbrides/orientbrides.user.js
@@ -7,6 +7,7 @@
 // @include      *Login.aspx*
 // @include      *default.aspx
 // @include      *agency.orientbrides.net/
+// @include      *type=newLetter
 // @grant        null
 // ==/UserScript==
 
@@ -14,7 +15,7 @@ function CollectEmail() {
   this.renderBase();
   this.initData();
   
-  if(localStorage.awaitPendingTimeout === 'true') {
+  if(localStorage.awaitPendingTimeout === 'true' && localStorage.platform === 'PC') {
     let refreshTimes = +localStorage.refreshTimes, selector;
     localStorage.refreshTimes = ++refreshTimes;
     localStorage.awaitPendingTimeout = 'false';
@@ -31,14 +32,13 @@ CollectEmail.prototype = {
   requestTimeSpan: 50,
   collectMaxId: 33914386,
   pendingLimit: 30,
-  awaitPendingTimeoutM: 2,
+  awaitPendingTimeoutM: 0.1,
   emailCurIdIndex: 0,
   ladyName: 'xiaojun',
   cantWriteFlag: 'Sorry, you can’t write letter to this man. Use presentations instead.',
   noInfoFlag: 'Information about a man is not available',
   nameFlag: '<td>Name:</td>',
   canWriteFlag: 'btnReply2',
-  clock: null,
   letterArr: [`I'm only five-foot-five and my hands are really small and sometimes I'm quiet and sometimes my face goes bright red really easily. Sometimes I drink too much and make a fool of myself but I hope you don't mind because I'll laugh at your jokes and I'd like you play with my hair or run your hand down the back of my arm. I know you know, and I know you are taking caution and you're trying to figure me out..\r\n\r\nI haven't figured me out, and I'll sincerely smile and wish you luck.\r\n\r\nPlease find me if you ever succeed.\r\n\r\nI'll fall in love with something obscure, the way your left elbow digs into your waist, almost unnoticeably, when you're nervous or the way you move the left side of your mouth when you are acting like what I say doesn't matter. We're all acting aren't we? \r\n\r\nI'm short and you're tall and I don't think it much matters anyway because I'll raise my shoulders for you so we can hold hands. If you hold my hand tight. If you make me feel like the greatest thing you've ever met, even just for that moment.\r\n\r\nBecause in this moment you are my greatest.`],
   awaitPendingTimeoutLimit() {
     let awaitPendingTimeoutM = this.awaitPendingTimeoutM;
@@ -60,37 +60,39 @@ CollectEmail.prototype = {
     let $ele = $(e.target);
     let text = $ele.text();
     let $table = this.$view.find('#table');
-    let setting,con;
+    let $setting,con;
     if (text === '设置') {
-      $table.attr('contenteditable', true)
+      $table.attr('contenteditable', true);
       $ele.text('保存');
     } else if (text === '保存') {
-      setting = ['awaitPendingTimeout','collectCurId','curTime','emailCurIdIndex','holdingPendingTimes','idArrayLength','idArray','pendingAmount','refreshTimes','theTypeOfTaskBeingPerformed','xhrFailTimes'];
-      setting.forEach(item=>{
-        if (item === 'idArray') {
+      $setting = this.$view.find('#table td');
+      $setting.each((index, item)=>{
+        let id = item.id;
+        if (id === 'idArray') {
           con = confirm('是否保存idArray?');
           if (con) {
-            localStorage[item] = this.$view.find(`#${item}`).text();
+            localStorage[id] = item.innerText;
           } else {
             return;
           }
+        } else {
+          localStorage[id] = item.innerText;
         }
-        localStorage[item] = this.$view.find(`#${item}`).text();
       });
-      $table.attr('contenteditable', false)
+      $table.attr('contenteditable', false);
       $ele.text('设置');
     }
   },
   renderBase() {
-    let $view = $(`<div>
-  <button class='btn btn-default' type='button' id='collectId'>开始扫描收集ID</button><br>
-  <button class='btn btn-default' type='button' id='sendEmail'>开始本地ID发信</button><br>
-  <button class='btn btn-default' type='button' id='scanIdSendEmail'>开始寻找ID发信</button><br>
+    let $view = $(`<div style='background:#fff;'>
+  <button class='btn btn-default' type='button' id='collectId'>开始扫描收集ID</button>
+  <button class='btn btn-default' type='button' id='sendEmail'>开始本地ID发信</button>
+  <button class='btn btn-default' type='button' id='scanIdSendEmail'>开始寻找ID发信</button>
   <button class='btn btn-default' type='button' id='setting'>设置</button>
 
-  <table id='table' style='margin-top:5px;outline:1px solid #000;background:#fff;border-collapse:collapse;' cellpadding="10" contenteditable="false">
+  <table id='table' style='margin-top:5px;outline:1px solid #000;background:#fff;border-collapse:collapse;width:100%;' cellpadding="10" contenteditable="false">
     <tr>
-      <th>awaitPendingTimeout</th>
+      <th>xhrFailTimes</th>
       <th>collectCurId</th>
       <th>curTime</th>
       <th>emailCurIdIndex</th>
@@ -98,12 +100,13 @@ CollectEmail.prototype = {
       <th>idArrayLength</th>
       <th>idArray</th>
       <th>pendingAmount</th>
+      <th>platform</th>
       <th>refreshTimes</th>
       <th>theTypeOfTaskBeingPerformed</th>
-      <th>xhrFailTimes</th>
+      <th>awaitPendingTimeout</th>
     </tr>
     <tr>
-      <td id='awaitPendingTimeout'></td>
+      <td id='xhrFailTimes'></td>
       <td id='collectCurId'></td>
       <td id='curTime'></td>
       <td id='emailCurIdIndex'></td>
@@ -111,18 +114,18 @@ CollectEmail.prototype = {
       <td id='idArrayLength'></td>
       <td id='idArray'></td>
       <td id='pendingAmount'></td>
+      <td id='platform'></td>
       <td id='refreshTimes'></td>
       <td id='theTypeOfTaskBeingPerformed'></td>
-      <td id='xhrFailTimes'></td>
+      <td id='awaitPendingTimeout'></td>
     </tr>
   </table>
+  <audio controls="controls" id="myMp3" loop="true" style='margin-top: 5px;'>
+    <source src="http://www.w3school.com.cn/i/song.mp3" type="audio/mpeg">
+    你的浏览器不支持audio标签
+  </audio>
+  <p id="lastId"></p>
 </div>`)
-    .css({
-      position: 'fixed',
-      top: '00px',
-      left: '10px',
-      'z-index': 999
-    })
     .find('button')
     .css({
       width: '150px',
@@ -177,6 +180,7 @@ CollectEmail.prototype = {
   },
   cCollectSend(e) {
     let $ele = $(e.target);
+    this.resetByBtn();
     $('#collectId').css('display', 'none');
     $('#sendEmail').css('display', 'none');
     if ($ele.text() === '开始寻找ID发信') {
@@ -185,11 +189,12 @@ CollectEmail.prototype = {
       this.mutilThreadCollect(true);
     } else if ($ele.text() === '暂停寻找ID发信') {
       $ele.text('开始寻找ID发信');
-      clearInterval(this.clock);
+      clearInterval(CollectEmail.clock);
     }
   },
   controlCollect(e) {
     let $ele = $(e.target);
+    this.resetByBtn();
     $('#sendEmail').css('display', 'none');
     $('#scanIdSendEmail').css('display', 'none');
     if ($ele.text() === '开始扫描收集ID') {
@@ -198,12 +203,13 @@ CollectEmail.prototype = {
       this.mutilThreadCollect();
     } else if ($ele.text() === '暂停扫描收集ID') {
       $ele.text('开始扫描收集ID');
-      clearInterval(this.clock);
+      clearInterval(CollectEmail.clock);
     }
   },
   controllEmail(e) {
     let $ele = $(e.target);
     let idArray = localStorage.idArray;
+    this.resetByBtn();
     window.idArray = JSON.parse(idArray);
     $('#collectId').css('display', 'none');
     $('#scanIdSendEmail').css('display', 'none');
@@ -213,8 +219,18 @@ CollectEmail.prototype = {
       this.mutilThreadEmail();
     } else if ($ele.text() === '暂停本地ID发信') {
       $ele.text('开始本地ID发信');
-      clearInterval(this.clock);
+      clearInterval(CollectEmail.clock);
     }
+  },
+  resetByBtn() {
+    if (localStorage.platform === 'ANDROID') localStorage.awaitPendingTimeout = 'false';
+    localStorage.xhrFailTimes = 0;
+    localStorage.refreshTimes = 0;
+    localStorage.pendingAmount = 0;
+    localStorage.pendingAmount = 0;
+    localStorage.pendingAmount = 0;
+    localStorage.pendingAmount = 0;
+    localStorage.holdingPendingTimes = this.awaitPendingTimeoutLimit();
   },
   getUrl(curId) {
     return `./ViewLadyCorrespondence.aspx?ladyID=${this.ladyId}&manID=${curId}&type=newLetter`;
@@ -246,6 +262,7 @@ CollectEmail.prototype = {
     if (localStorage.letterArr === undefined) localStorage.letterArr = JSON.stringify(this.letterArr);
     if (localStorage.awaitPendingTimeout === undefined) localStorage.awaitPendingTimeout = false;
     if (localStorage.refreshTimes === undefined) localStorage.refreshTimes = 0;
+    if (localStorage.platform === undefined) localStorage.platform = 'PC';
     if (localStorage.collectCurId === undefined) alert('collectCurId未设置');
     if (localStorage.emailCurIdIndex === undefined) alert('emailCurIdIndex未设置');
     if(localStorage.idArray === undefined) {
@@ -262,6 +279,7 @@ CollectEmail.prototype = {
     return new Promise((resolve, reject)=>{
       let url = this.getUrl(id);
       $.ajax({url:url,type:'GET'}).done(((data,statusText)=>{
+        this.reduceXhrFailTimes();
         if (typeof data !== 'string') reject('fail');
         let startIndex = data.indexOf(this.nameFlag);
         startIndex += this.nameFlag.length;
@@ -284,16 +302,18 @@ CollectEmail.prototype = {
     });
   },
   handleReachPendingLimit(field) {
-    let pendingAmount = +localStorage[field];
-    localStorage[field] = --pendingAmount;
-    if (pendingAmount === 0) this.refresh();
+    let pendingTimes = +localStorage[field];
+    if (pendingTimes > 0) localStorage[field] = --pendingTimes;
+    if (pendingTimes === 0) {
+      this.refresh();
+    }
   },
   resetHoldingPendingTimes(field) {
     localStorage[field] = this.awaitPendingTimeoutLimit();
   },
   mutilThreadEmail() {
     let timeSpan = this.requestTimeSpan;
-    this.clock = setInterval(()=>{
+    CollectEmail.clock = setInterval(()=>{
       let pendingAmount = +localStorage.pendingAmount;
       let curId, selector, btnText, completeText, letter, letterCurIndex, letterArr, emailCurIdIndex;
       selector = '#sendEmail';
@@ -302,7 +322,7 @@ CollectEmail.prototype = {
       emailCurIdIndex = +localStorage.emailCurIdIndex;
       if (pendingAmount <= this.pendingLimit) {
         if (emailCurIdIndex > window.idArray.length) {
-          clearInterval(this.clock);
+          clearInterval(CollectEmail.clock);
           alert(completeText);
           $(selector).text(btnText);
           return;
@@ -335,7 +355,7 @@ CollectEmail.prototype = {
   },
   mutilThreadCollect(sendEmail) {
     let timeSpan = this.requestTimeSpan;
-    this.clock = setInterval(()=>{
+    CollectEmail.clock = setInterval(()=>{
       let pendingAmount = +localStorage.pendingAmount;
       let maxId, curId, selector, btnText, completeText, nexId;
       maxId = this.collectMaxId;
@@ -349,7 +369,7 @@ CollectEmail.prototype = {
         nexId = this.increaseId(curId);
         this.saveCurId(nexId);
         if (curId > maxId) {
-            clearInterval(this.clock);
+            clearInterval(CollectEmail.clock);
             $(selector).text(btnText);
             alert(completeText);
             return;
@@ -382,9 +402,10 @@ CollectEmail.prototype = {
       ctl00$ctl00$ctl00$ContentPlaceHolder1$nestedContentPlaceHolder$ContentIndex$cntrlViewLadyCorrespondence$btnLetterReplySend:'Send'
       }
     }).done((data,statusText)=>{
-      localStorage.xhrFailTimes = 0;
+      this.reduceXhrFailTimes();
       if (sendEmail === true) this.msg('寻找ID发信', `已发送 ${id}`);
-      if (sendEmail === undefined) this.msg('开始本地ID发信', `已发送 ${id}`);
+      if (sendEmail === undefined) this.msg('本地ID发信', `已发送 ${id}`);
+      this.$view.find('#lastId').text(`最近一次发送的时间与ID: ${new Date()} ${id}`);
     }).fail(xhr=>{
       this.handleFail(xhr, 'xhrFailTimes', this.pendingLimit, id, this.refresh);
     }).always(()=>{
@@ -393,12 +414,18 @@ CollectEmail.prototype = {
       this.resetHoldingPendingTimes('holdingPendingTimes');
     });
   },
+  reduceXhrFailTimes() {
+    let xhrFailTimes = localStorage.xhrFailTimes;
+    if (xhrFailTimes > 0)localStorage.xhrFailTimes = --xhrFailTimes;
+    this.$view.find('#xhrFailTimes').text = xhrFailTimes;
+  },
   doCollect(id, sendEmail) {
     let url = this.getUrl(id);
     $.ajax({url:url,type:'GET'}).done(((data,statusText)=>{
+      this.reduceXhrFailTimes();
       let idArray,nextId,idJSON,cantWriteIndex,canWriteIndex,noInfoIndex, startIndex, endIndex, manName, letterCurIndex, letterArr, letter, pendingAmount, emailCurIdIndex;
       let cantWriteFlag = this.cantWriteFlag;
-      localStorage.xhrFailTimes = 0;
+      this.reduceXhrFailTimes();
       if (typeof data === 'string') {
         cantWriteIndex = data.indexOf(cantWriteFlag);
         canWriteIndex = data.indexOf(this.canWriteFlag);
@@ -444,9 +471,27 @@ CollectEmail.prototype = {
     if (failTimes > limit) fn();
   },
   refresh() {
-    clearInterval(this.clock);
+    let platform = localStorage.platform;
+    let theTypeOfTaskBeingPerformed = localStorage.theTypeOfTaskBeingPerformed;
+    if (localStorage.awaitPendingTimeout === 'true') return;
+    clearInterval(CollectEmail.clock);
+    if (platform === 'PC') {
+      window.location.reload(true);
+    } else if (platform === 'ANDROID') {
+      $('#myMp3').get(0).play();
+    }
+    switch(theTypeOfTaskBeingPerformed) {
+      case 'scanIdSendEmail':
+        $('#scanIdSendEmail').text('开始寻找ID发信');
+        break;
+      case 'sendEmail':
+        $('#sendEmail').text('开始本地ID发信');
+        break;
+      case 'collectId':
+        $('#collectId').text('开始扫描收集ID');
+        break;
+    }
     localStorage.awaitPendingTimeout = 'true';
-    window.location.reload(true);
   },
   msg(title,content,iconUrl='https://raw.githubusercontent.com/YLJ77/Tampermonkey/master/img/icon128.png') {
     let _this = this;
@@ -498,14 +543,13 @@ function Login() {
 
 Login.prototype.findEle = CollectEmail.prototype.findEle;
 
-
 (()=>{
   let href = window.location.href;
   if (href.indexOf('Login.aspx') !== -1) {
     new Login();
-  } else if (href.indexOf('copyPage') === -1) {
-      window.open(href+'?copyPage');
-    } else {
-      new CollectEmail();
-    }
+  } else if (href.indexOf('default.aspx') !== -1 || href === 'http://agency.orientbrides.net/') {
+    window.location.href = 'http://agency.orientbrides.net/index/ViewLadyCorrespondence.aspx?ladyID=1187357&manID=11111111&type=newLetter';
+  } else if (href.indexOf('type=newLetter') !== -1) {
+    new CollectEmail();
+  }
 })();
