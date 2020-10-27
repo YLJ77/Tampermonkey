adb7c1165046ceb2593484953b2abfd016e52732
YLJ <linjieyang77@gmail.com>
2020-10-27 19:04:33 +0800

    feat:添加less配置

--- job/vue.config.js
+++ job/vue.config.js
@@ -1,24 +1,62 @@
 const webpack = require('webpack');
+const StringReplacePlugin = require("string-replace-webpack-plugin");
+const path = require('path');
+
 module.exports = {
     css: {
         extract: false,
     },
-    configureWebpack: {
-        devServer: {
-            disableHostCheck: true,
-            writeToDisk: true // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
-        },
-        output: {
-            filename: 'job_list.user.js',
-        },
-        optimization: {
-            splitChunks: false
-        },
+    configureWebpack: config => {
+        const lessRule = config.module.rules.find(entry => entry.test.source === '\\.less$')
+        // lessRule && (lessRule.exclude = [path.resolve(__dirname,'node_modules/ant-design-vue/es/style/core/base.less')]);
+        if (lessRule) {
+            // lessRule.enforce = 'pre';
+            lessRule.oneOf.forEach(entry => {
+                const {use} = entry;
+                const lastUse = use[use.length - 1];
+                lastUse.options.additionalData = (content, loaderContext) => {
+                    // More information about available properties https://webpack.js.org/api/loaders/
+                    const { resourcePath, rootContext } = loaderContext;
+                    const relativePath = path.relative(rootContext, resourcePath);
+    // console.log(`
+    // '${relativePath}'`);
+if (relativePath === 'node_modules\\ant-design-vue\\es\\style\\index.less') {
+    console.log(relativePath);
+    console.log(content);
+}
+                    // if (relativePath === 'styles/foo.less') {
+                    //   return '@value: 100px;' + content;
+                    // }
+    
+                    // return '@value: 200px;' + content;
+                    return content;
+                  }
+            })
+        }
+        return {
+            devServer: {
+                disableHostCheck: true,
+                writeToDisk: true // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+            },
+            output: {
+                filename: 'job_list.user.js',
+            },
+            optimization: {
+                splitChunks: false
+            },
+        }
     },
     productionSourceMap: false,
     publicPath: './',
     filenameHashing: false,
     chainWebpack: config => {
+        // config.module
+        // .rule('null-loader')
+        // .test(path.resolve(__dirname,'node_modules/ant-design-vue/es/style/core/base.less'))
+        // .use('null-loader')
+        // .loader('null-loader')
+        // .end()
+
         config
             .plugin('banner')
             .use(webpack.BannerPlugin,[{
