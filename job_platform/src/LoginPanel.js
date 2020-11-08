4eb65ec1871f04c222c945191d7f3d7c5c53d670
yanglinjie <linjieyang77@gmail.com>
2020-11-08 20:51:52 +0800

    feat:cross window communicate

--- job_platform/src/LoginPanel.js
+++ job_platform/src/LoginPanel.js
@@ -3,11 +3,15 @@ import 'src/assets/loginPanel.less'
 export class LoginPanel {
     constructor() {
         window.addEventListener("message", (event) => {
-            console.warn('---------------lagou:');
-            console.warn(event);
+            if (event.data.token) {
+                localStorage.setItem('token',event.data.token);
+            }
         }, false);
+        // this.visibleLoginPanel();
+    }
+    visibleLoginPanel() {
         // const win = window.open('http://219.136.175.12:3000','','width=400,height=300');
-        window.open('http://192.168.43.218:8081','','width=400,height=300');
+        window.open('http://127.0.0.1:8080','','width=400,height=300');
     }
 
 
